dbb71081f4637c103fa7db9063afbed5510f5c1c
Dominique Demetz <ddemetz123@icloud.com>
2018-06-05 01:43:33 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -8,9 +8,8 @@ def display_rainbow(colors)
   print "G: #{colors [3]}, "
   print "B: #{colors [4]}, "
   print "I: #{colors [5]}, "
-  print "V: #{colors [6]}"
+  print "V: #{colors [6]}/n"
 end
 
 display_rainbow (['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
   
-R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet
