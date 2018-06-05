1eb9e8188aae4dd4de8394f688343a50ae02ddfb
Dominique Demetz <ddemetz123@icloud.com>
2018-06-05 01:37:32 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,7 +2,7 @@
 
 
 def display_rainbow(colors)
-  print "R: #{colors [0]}, O: #{colors [1]}, Y: #{colors [2]},
+  puts "R: #{colors [0]}, O: #{colors [1]}, Y: #{colors [2]},
   G: #{colors [3]}, B: #{colors [4]}, I: #{colors [4]}, V: #{colors [5]}"
 end
 
